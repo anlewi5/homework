4ed1f3d713c70e02a8d7e4c7752f874e064c882b
Anna Lewis <anna.noel.lewis@gmail.com>
2017-08-14 17:41:32 -0600

    Add consise solution to super_fizz problem.

--- super_fizz.rb
+++ super_fizz.rb
@@ -1,4 +1,6 @@
-(0..105).each { |number|
+#clear version
+
+(0..1000).each { |number|
   if number % 3 === 0 && number % 5 === 0 && number % 7 === 0
     puts "SuperFizzBuzz"
   elsif number % 3 === 0 && number % 7 === 0
@@ -16,4 +18,22 @@
   else
     puts number.to_s
   end
+}
+
+#concise version
+
+def get_words(number)
+  word = ""
+  word += "Super" if number % 7 === 0
+  word += "Buzz" if number % 5 === 0
+  word += "Fizz" if number % 3 === 0
+  if word.empty?
+    puts number.to_s
+  else
+    puts word
+  end
+end
+
+(0..1000).each { |number|
+  get_words(number)
 }
