0e397261efc7441445ea21483d77d15b83c1e09e
Anna Lewis <anna.noel.lewis@gmail.com>
2017-08-14 17:09:18 -0600

    Add working super_fizz exercise.

---
+++ super_fizz.rb
@@ -0,0 +1,19 @@
+(0..105).each { |number|
+  if number % 3 === 0 && number % 5 === 0 && number % 7 === 0
+    puts "SuperFizzBuzz"
+  elsif number % 3 === 0 && number % 7 === 0
+    puts "SuperFizz"
+  elsif number % 5 === 0 && number % 7 === 0
+    puts "SuperFizzBuzz"
+  elsif number % 3 === 0 && number % 5 === 0
+    puts "FizzBuzz"
+  elsif number % 3 === 0
+    puts "Fizz"
+  elsif number % 5 === 0
+    puts "Buzz"
+  elsif number % 7 === 0
+    puts "Super"
+  else
+    puts number.to_s
+  end
+}
