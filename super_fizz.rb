b135da14f1ed9f99d6a42588867934b254113924
Anna Lewis <anna.noel.lewis@gmail.com>
2017-08-15 15:39:01 -0600

    Add class refactored solution

--- super_fizz.rb
+++ super_fizz.rb
@@ -40,34 +40,59 @@
 
 #class clear solution
 
-1001.times do |num|
-  def divisible_by_3?(num)
-    num % 3 == 0
-  end
-
-  def divisible_by_5?(num)
-    num % 5 == 0
-  end
-
-  def divisible_by_7?(num)
-    num % 7 == 0
-  end
-
-  if divisible_by_3?(num) && divisible_by_5?(num) && divisible_by_7?(num)
-    puts "SuperFizzBuzz"
-  elsif divisible_by_3?(num) && divisible_by_7?(num)
-    puts "SuperFizz"
-  elsif divisible_by_5?(num) && divisible_by_7?(num)
-    puts "SuperBuzz"
-  elsif divisible_by_3?(num) && divisible_by_5?(num)
-    puts "Fizz"
-  elsif divisible_by_5?(num)
-    puts "Buzz"
-  elsif divisible_by_7?(num)
-    puts "Super"
-  else
-    puts num
-  end
+def divisible_by_3?(num)
+  num % 3 == 0
 end
 
+def divisible_by_5?(num)
+  num % 5 == 0
+end
+
+def divisible_by_7?(num)
+  num % 7 == 0
+end
+
+# 1001.times do |num|
+#   if divisible_by_3?(num) && divisible_by_5?(num) && divisible_by_7?(num)
+#     puts "SuperFizzBuzz"
+#   elsif divisible_by_3?(num) && divisible_by_7?(num)
+#     puts "SuperFizz"
+#   elsif divisible_by_5?(num) && divisible_by_7?(num)
+#     puts "SuperBuzz"
+#   elsif divisible_by_3?(num) && divisible_by_5?(num)
+#     puts "FizzBuzz"
+#   elsif divisible_by_3?(num)
+#     puts "Fizz"
+#   elsif divisible_by_5?(num)
+#     puts "Buzz"
+#   elsif divisible_by_7?(num)
+#     puts "Super"
+#   else
+#     puts num
+#   end
+# end
+
 #class refactored solution
+
+1001.times do |num|
+  output = ""
+
+  if divisible_by_7?(num)
+    output = output + "Super"
+  end
+
+  if divisible_by_3?(num)
+    output = output + "Fizz"
+  end
+
+  if divisible_by_5?(num)
+    output = output + "Buzz"
+  end
+
+  if output.empty?
+    output = num
+  end
+
+  puts output
+
+end
